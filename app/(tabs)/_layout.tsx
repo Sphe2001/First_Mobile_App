168e276e1a8a9fe97fe1c75e1b0d7c7d76235a2a
Siphelele Miya <161841132+Sphe2001@users.noreply.github.com>
2025-03-17 15:27:15 +0200

    Changed the tab bar styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,23 @@ const _layout = () => {
         <Tabs
             screenOptions={{
                 tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                tabBarStyle: {
+                    backgroundColor: "#0F0D23",
+                    borderRadius: 50,
+                    marginHorizontal: 20,
+                    marginBottom: 25,
+                    height: 52,
+                    position: "absolute",
+                    overflow: "hidden",
+                    borderWidth: 1,
+                    borderColor: "#0F0D23",
+                },
             }}>
             <Tabs.Screen
                 name={"index"}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <View className={"flex-1 justify-center items-center"}
     >
-      <Text className={"text-5xl text-accent font-bold"}>Hello world!</Text>
+      <Text className={"text-5xl text-secondary font-bold"}>Home Page</Text>
     </View>
   );
 }
